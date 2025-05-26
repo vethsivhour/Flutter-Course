0634c3fc1a64169a09ee4238c6898594470b809b
vsivhour <vethsivhour@gmail.com>
2025-05-26 23:42:13 +0700

    init flutter create .

---
+++ android/app/src/main/kotlin/com/example/bitcoin_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bitcoin_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- lib/utilities/users_service.dart
+++ lib/utilities/users_service.dart
@@ -1,25 +1,66 @@
-// TODO-2: Import http and dart:convert
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class UsersService {
   static final UsersService _singleton = UsersService._internal();
   UsersService._internal();
   static UsersService get instance => _singleton;
 
-  // TODO-3: Add base URL
+  final String baseUrl = 'https://6574db93b2fbb8f6509cbae5.mockapi.io';
 
   Future<List<dynamic>> getUsers() async {
-    // TODO-4: Fetch all users
+    try {
+      final response = await http.get(Uri.parse('$baseUrl/users'));
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      }
+      throw Exception('Unable to retrieve users');
+    } catch (e) {
+      throw Exception('Failed to fetch users: $e');
+    }
   }
 
   Future<dynamic> updateUser(String id, Map<String, dynamic> data) async {
-    // TODO-5: Update a user
+    try {
+      final response = await http.patch(
+        Uri.parse('$baseUrl/users/$id'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(data),
+      );
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      }
+      throw Exception('Unable to update user');
+    } catch (e) {
+      throw Exception('Failed to update user: $e');
+    }
   }
 
   Future<dynamic> createUser(Map<String, dynamic> data) async {
-    // TODO-6: Create new user
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/users'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(data),
+      );
+      if (response.statusCode == 201) {
+        return jsonDecode(response.body);
+      }
+      throw Exception('Unable to create user');
+    } catch (e) {
+      throw Exception('Failed to create user: $e');
+    }
   }
 
   Future<dynamic> deleteUser(String id) async {
-    // TODO-7: Delete a user
+    try {
+      final response = await http.delete(Uri.parse('$baseUrl/users/$id'));
+      if (response.statusCode == 200) {
+        return true;
+      }
+      throw Exception('Unable to delete user');
+    } catch (e) {
+      throw Exception('Failed to delete user: $e');
+    }
   }
 }
